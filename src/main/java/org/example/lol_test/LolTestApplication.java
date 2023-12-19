04c33a63a2e1fed60204c20ca0baf51b1fee53ff
wjstmddk <82574092+wjstmddk@users.noreply.github.com>
2023-12-19 11:07:58 +0900

    test

---
+++ src/main/java/org/example/lol_test/LolTestApplication.java
@@ -0,0 +1,13 @@
+package org.example.lol_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LolTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/lol_test/LolTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lol_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

9574a0d98db539860c2ff931d70ee07a244c1812
takealook <82574092+wjstmddk@users.noreply.github.com>
2024-01-08 15:25:03 +0900

    commit

--- src/main/java/org/example/lol_test/controller/MainController.java
+++ src/main/java/org/example/lol_test/controller/MainController.java
@@ -1,9 +1,14 @@
 package org.example.lol_test.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+    @GetMapping("/main/game")
+    public String searchgame(){
 
+        return "resultGame";
+    }
 
 }
